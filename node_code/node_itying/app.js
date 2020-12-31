6a9fad22fc8c0dfab35a9e8a0277e445c6154a8d
root <root@hwsrv-670816.hostwindsdns.com>
2020-12-31 09:02:49 +0800

    .

--- node_code/node_itying/app.js
+++ node_code/node_itying/app.js
@@ -77,3 +77,49 @@ const fs = require("fs");
   //     if (err) console.log(err);
   //   });
 }
+
+{ 
+  // 08
+  /**
+ * 以流的方式读取文件
+ */
+  // const readStream = fs.createReadStream('./assets/input.txt');
+
+  // let count = 0;
+  // let str = ''
+  // readStream.on('data', data => {
+  //   str += data
+  //   ++count
+  // })
+
+  // readStream.on('end', _ => {
+  //   console.log(str)
+  //   console.log(count)
+  // })
+
+  // readStream.on('error', err => {
+  //   console.error(err)
+  // })
+
+
+  /**
+   * 以流的方式写入文件
+   */
+  // const writeStream = fs.createWriteStream('./assets/output.txt')
+
+  // let data = ''
+  // for(let i = 0; i < 500; i++)  data += '导航模式'
+
+
+  // writeStream.write(data, 'utf-8')
+
+  // writeStream.end() // 标记写入完成
+
+  // writeStream.on('finish', () => {
+  //   console.log('写入完成')
+  // })
+
+  // writeStream.on('error', err => {
+  //   console.error(err)
+  // })
+}
